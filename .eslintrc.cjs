e7a58f1326b429ccd0b44243c7b265e545255443
IamHi <inpactt@gmail.com>
2025-11-27 20:41:03 +0100

    Initial, Iteration1, Interation2

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {
+        // Astro-specific rules here
+      },
+    },
+  ],
+};
